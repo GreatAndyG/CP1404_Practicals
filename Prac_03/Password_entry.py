1c90536ac7f93e5697af3820494444a8552fb404
Andrew Gibson <Andrew.gibson@my.jcu.edu.au>
2019-03-27 18:12:00 +1000

    Add program.

---
+++ Prac_03/Password_entry.py
@@ -0,0 +1,8 @@
+MINIMUM_LENGTH = 6
+
+"""Get password of valid size from user and print asterisks"""
+password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+
+print('*' * len(password))
