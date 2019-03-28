9572b147deaabf4961d13b06a0ab15704ab00513
Andrew Gibson <Andrew.gibson@my.jcu.edu.au>
2019-03-28 10:17:22 +1000

    Refactor password entry program to use functions

--- Prac_03/Password_entry.py
+++ Prac_03/Password_entry.py
@@ -1,8 +1,21 @@
 MINIMUM_LENGTH = 6
 
-"""Get password of valid size from user and print asterisks"""
-password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-while len(password) < MINIMUM_LENGTH:
-    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
 
-print('*' * len(password))
+def main():
+    """Get password of valid size from user and print asterisks"""
+    print_password()
+
+
+def print_password():
+    password = get_password()
+    print('*' * len(password))
+
+
+def get_password():
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    return password
+
+
+main()
