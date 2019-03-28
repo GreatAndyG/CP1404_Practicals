1de9c781d3874130db0cb510874973f8ae14699e
jc342795 <Andrew_gibson@my.jcu.edu.au>
2019-03-28 13:40:32 +1000

    Add format structure and make print function a function

--- Prac_04/total_income.py
+++ Prac_04/total_income.py
@@ -7,15 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    Number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, Number_of_months + 1):
+        income = float(input("Enter income for month {} ".format(month)))
         incomes.append(income)
 
-    print("\nIncome Report\n-------------")
+    print_income_report(Number_of_months, incomes)
+
+
+def print_income_report(Number_of_months, incomes):
+    print("\nIncome Report\n------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, Number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
