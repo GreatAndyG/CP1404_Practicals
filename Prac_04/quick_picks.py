24a74cd64b570ef997be8e55bf4e59177bb9d0f1
jc342795 <Andrew_gibson@my.jcu.edu.au>
2019-04-04 13:22:02 +1000

    complete code for quick_picks

---
+++ Prac_04/quick_picks.py
@@ -0,0 +1,26 @@
+import random
+
+numbers_per_line = 6
+minimum = 1
+maximum = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How may quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Cannot be less than 0")
+        number_of_quick_picks = int(input("How many quick picks "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for p in range(numbers_per_line):
+            number = random.randint(minimum, maximum)
+            while number in quick_pick:
+                number = random.randint(minimum, maximum)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
