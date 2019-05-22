7ae21b26a7a836bb590b8b0984d5ea5364d39f90
Andrew Gibson <Andrew.gibson@my.jcu.edu.au>
2019-05-22 17:50:54 +1000

    Prac_09 intermediate exercise commit.

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -19,7 +19,14 @@ def main():
     # Make a new directory
     # The next time you run this, it will crash if the directory exists
     # TODO: Use exception handling to avoid the crash (just pass)
-    os.mkdir('temp')
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+
+
+
+
 
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
@@ -31,11 +38,11 @@ def main():
         print("Renaming {} to {}".format(filename, new_name))
 
         # TODO: Try these options one at a time
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
+        #rename file to new name - in place
+        #os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+        #shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -54,7 +61,12 @@ def demo_walk():
         print("(Current working directory is: {})".format(os.getcwd()))
 
         # TODO: add a loop to rename the files
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(full_name, new_name)
+            
 
 
-main()
-# demo_walk()
+#main()
+demo_walk()
